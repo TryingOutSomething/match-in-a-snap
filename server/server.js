c90a2ecdae5acd4d3426dcaa2239e7e6f2a91a46
TryingOutSomething <jan.owyeong123@gmail.com>
2020-07-07 14:10:13 +0800

    added basic security to backend

--- server/server.js
+++ server/server.js
@@ -1,26 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const helmet = require('helmet');
 const routes = require('./routers/routes');
 
 let app = express();
 
+app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
 app.use('/api/', routes);
-app.use((error, req, res) => {
-  if (error.status) {
-    res.status(error.status);
-  } else {
-    res.status(500);
-  }
-
-  res.json({
-    message: error.message,
-    stack: process.env.NODE_ENV === 'production' ? 'Error occurred' : error.stack
-  });
+app.use((error, req, res, next) => {
+  res.status(error.status || 500)
+     .json({
+       message: error.message,
+       stack: process.env.NODE_ENV === 'production' ? 'Error occurred' : error.stack
+     });
 });
+
+app.disable('x-powered-by');
 
 module.exports = app;
